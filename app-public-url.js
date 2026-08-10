7be2ba016d94128f217ad5622799c90ef7630708
umakanthjopwer-beep <umakanth.jopwer@gmail.com>
2026-08-10 07:43:43 +0530

    Use stable public URL and refresh PWA worker

--- app-public-url.js
+++ app-public-url.js
@@ -1,2 +1,6 @@
 const PUBLIC_APP_URL="https://githubraw.com/umakanthjopwer-beep/syllabus/main/index.html";
 appLink=function(){return PUBLIC_APP_URL};
+
+if("serviceWorker" in navigator){
+  navigator.serviceWorker.register("sw.js?v=10",{updateViaCache:"none"}).catch(err=>console.warn("Service worker registration skipped",err));
+}
