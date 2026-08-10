de0a5eee52976ff04fa54d5af0975d5cce60b87f
umakanthjopwer-beep <umakanth.jopwer@gmail.com>
2026-08-10 09:41:38 +0530

    Switch permanent app URL to Cloudflare Pages

--- app-public-url.js
+++ app-public-url.js
@@ -1,6 +1,6 @@
-const PUBLIC_APP_URL="https://syllabuslagging.umakanth-jopwer.workers.dev";
+const PUBLIC_APP_URL="https://syllabuslagging.pages.dev";
 appLink=function(){return PUBLIC_APP_URL};
 
 if("serviceWorker" in navigator){
-  navigator.serviceWorker.register("sw.js?v=10",{updateViaCache:"none"}).catch(err=>console.warn("Service worker registration skipped",err));
+  navigator.serviceWorker.register("sw.js?v=11",{updateViaCache:"none"}).catch(err=>console.warn("Service worker registration skipped",err));
 }
