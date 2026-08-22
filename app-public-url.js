b929a34b95976d68e6b12e22de4de7187e625d60
umakanthjopwer-beep <umakanth.jopwer@gmail.com>
2026-08-22 07:30:06 +0530

    Register service worker v69

--- app-public-url.js
+++ app-public-url.js
@@ -2,7 +2,7 @@ const PUBLIC_APP_URL="https://syllabuslagging.pages.dev";
 appLink=function(){return PUBLIC_APP_URL};
 
 if("serviceWorker" in navigator){
-  navigator.serviceWorker.register("sw.js?v=68",{updateViaCache:"none"}).catch(err=>console.warn("Service worker registration skipped",err));
+  navigator.serviceWorker.register("sw.js?v=69",{updateViaCache:"none"}).catch(err=>console.warn("Service worker registration skipped",err));
 }
 
 window.yearPlanRepairReady=(async()=>{
